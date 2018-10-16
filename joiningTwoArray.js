670ffca782d2a49ebd2ad502a91afd04f1f64274
Mustafa <mustafamfaisal@gmail.com>
2018-10-16 22:44:53 +0300

    remove papa parser package

--- joiningTwoArray.js
+++ joiningTwoArray.js
@@ -3,7 +3,7 @@ var array1 = ["a", "b", "c", "d"],
     array2 = [1, 2, 3, 4],
     result = [],
     i, l = Math.min(array1.length, array2.length);
-// for loop to eterate through the two arrays and push each elemant     
+// for loop to eterate through the two arrays and push each elemant
 for ( i = 0; i < l; i++) {
     result.push(array1[i], array2[i]);
 }
